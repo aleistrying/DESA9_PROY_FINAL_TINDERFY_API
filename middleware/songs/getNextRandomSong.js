1d6d8ac69fd2d7711e3c89373d55507f06be0972
Alejandro P <alejandroparparcen@yahoo.com>
2022-07-23 11:42:39 -0500

    finishing touches on handle next songs

--- middleware/songs/getNextRandomSong.js
+++ middleware/songs/getNextRandomSong.js
@@ -7,22 +7,31 @@ module.exports = async (req, res) => {
         const subscription = await Subscriptions.findOne({
             userId: req.user._id,
             isActive: true,
-        })
+        }).populate("subscriptionTypeId");
+        // console.log(req.user.dailySongsRequested >= req.user.songLimit,
+        //     req.user.dailySongsRequested >= subscription.subscriptionTypeId.maxSwipes,
+        //     {
+        //         dailySongsRequested: req.user.dailySongsRequested,
+        //         songLimit: req.user.songLimit,
+        //         maxSwipes: subscription.maxSwipes,
+
+        //     })
         if (!subscription) {
             if (req.user.dailySongsRequested >= req.user.songLimit)
                 return res.status(200).send({ success: false, error: "Has llegado al limite de canciones que puedes escuchar en tu plan. Compra premium si quieres continuar", pay: true });
-        } else if (req.user.dailySongsRequested >= subscription.maxSwipes)
+        } else if (req.user.dailySongsRequested >= subscription.subscriptionTypeId.maxSwipes)
             return res.status(200).send({ success: false, error: "Has llegado al limite de canciones que puedes escuchar en tu plan. Compra premium++ si quieres continuar", pay: true });
 
         const swipedSongs = await SwipedSongs.find({
             userId: req.user._id,
-        });
+        }).sort({ createdAt: -1 }).lean();
 
         const songs = await Songs.find({
-            _id: { $nin: swipedSongs },
+            _id: { $nin: swipedSongs.map(swipe => swipe.songId) },
         });
+
         if (songs.length === 0)
-            return res.status(200).send({ success: false, error: "No hay canciones disponibles" });
+            return res.status(200).send({ success: false, error: "No hay nuevas canciones disponibles para tí" });
 
         const song = songs[Math.floor(Math.random() * songs.length)];
 
